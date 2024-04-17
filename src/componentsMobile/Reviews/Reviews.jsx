bc077326b50d0f7139dfef36721b0be0f91cd954
Evgeni67 <evgeni313@abv.bg>
2024-04-17 18:11:41 +0300

    type

--- src/componentsMobile/Reviews/Reviews.jsx
+++ src/componentsMobile/Reviews/Reviews.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import styles from "./Reviews.module.scss"; // Import the Sass file for styling
 
-import ReviewsImage from '../../assets/images/ReviewsImage.jpg'
+import ReviewsImage from "../../assets/images/ReviewsImage.jpg";
 
 const Reviews = ({ targetElementRef }) => {
   const reviews = [
     {
       id: 1,
-      text: "„Изключително бърза и професионална смяна на гуми и реклаж на джанти. Много удобна услуга- без висене пред сервизи за смяна на гуми“",
+      text: "„Изключително бърза и професионална смяна на гуми. Много удобна услуга- без висене пред сервизи за смяна на гуми“",
     },
     {
       id: 2,
@@ -23,17 +23,13 @@ const Reviews = ({ targetElementRef }) => {
     <div className={styles.reviewsContainer} ref={targetElementRef}>
       <div className={styles.Text}>Ревюта</div>
       <div className={styles.reviewsContainerRight}>
-        <img alt ='test' 
-          src={
-            ReviewsImage
-          }
-          className={styles.ImageReview}
-        />
+        <img alt="test" src={ReviewsImage} className={styles.ImageReview} />
       </div>
       <div className={styles.reviewsContainerLeft}>
         {reviews.map((review) => (
           <div key={review.id} className={styles.reviewCard}>
-            <img alt ='test' 
+            <img
+              alt="test"
               className={styles.snimkaKaka}
               src={
                 "https://mobilexpert-gumi.com/wp-content/uploads/2019/12/stars-5.png"
